d7a30f954a33df7ef9abcbbcd4e579932e75baaa
Traker01 <kamil1e7@interia.pl>
2020-04-29 00:55:12 +0200

    Cwiczenie 1

---
+++ Aplikacja_1/drugaczesc.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Aplikacja_1
+{
+    partial class drugaczesc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labeltitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labeltitle
+            // 
+            this.labeltitle.AutoSize = true;
+            this.labeltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labeltitle.Location = new System.Drawing.Point(36, 143);
+            this.labeltitle.Name = "labeltitle";
+            this.labeltitle.Size = new System.Drawing.Size(0, 55);
+            this.labeltitle.TabIndex = 0;
+            // 
+            // drugaczesc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labeltitle);
+            this.Name = "drugaczesc";
+            this.Text = "drugaczesc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labeltitle;
+    }
+}
